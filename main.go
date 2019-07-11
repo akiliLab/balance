0f629bec0c0f4366415ebf4ef6d4709047dcfc81
MAKOSCAFEE <barnabasmakonda@gmail.com>
2019-07-11 20:49:01 +0300

    specify port that will be used to run gprc server

--- main.go
+++ main.go
@@ -2,17 +2,54 @@ package main
 
 import (
 	"flag"
+	"time"
+
+	"github.com/micro/go-log"
+
+	handler "github.com/akililab/balance/handler"
+	subscriber "github.com/akililab/balance/subscriber"
+	"github.com/micro/go-micro"
+	"github.com/micro/go-micro/service/grpc"
+
+	balance "github.com/akililab/balance/proto/balance"
 )
 
 var (
 	serverAddr string
-	callAddr   string
-	cmdHelp    bool
 )
 
 func init() {
 	flag.StringVar(&serverAddr, "server_address", "0.0.0.0:9080", "server address.")
-	flag.StringVar(&callAddr, "client_call_address", ":9080", "client call options address.")
-	flag.BoolVar(&cmdHelp, "h", false, "help")
 	flag.Parse()
+}
+
+func main() {
+
+
+	// New Service
+	service := grpc.NewService(
+		micro.Name("go.micro.srv.balance"),
+		micro.RegisterTTL(time.Second*30),
+		micro.Address(serverAddr),
+		micro.RegisterInterval(time.Second*10),
+	)
+
+	// optionally setup command line usage
+	service.Init()
+
+	// Register Handlers
+	balance.RegisterBalanceServiceHandler(service.Server(), new(handler.Balance))
+
+	// Register Struct as Subscriber
+	micro.RegisterSubscriber("topic.go.micro.srv.balance", service.Server(), new(subscriber.Balance))
+
+	// Register Function as Subscriber
+	micro.RegisterSubscriber("topic.go.micro.srv.balance", service.Server(), subscriber.Handler)
+
+	log.Logf("Service Run")
+
+	// Run server
+	if err := service.Run(); err != nil {
+		log.Fatal(err)
+	}
 }
